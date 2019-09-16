4c2860e9aef4ff7450f2b4c946bdfac9a09e92a5
William Jeffers <ilovesnowmen93@hotmail.com>
2019-09-16 14:07:10 -0700

    Game Manager Time
    yeet

---
+++ Tank Game/Assets/Scripts/TestController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+	public static GameManager instance;
+
+
+	void Awake()
+	{
+		if (instance == null) {instance = this;}
+		else {Debug.LogError("There can only be one"); Destroy(gameObject);}
+	}
+}
